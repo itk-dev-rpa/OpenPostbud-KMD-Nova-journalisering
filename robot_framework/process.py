4a1f419f9fedd09d7c9ac6e71b2b289eecaca818
Mathias G <ghbm@aarhus.dk>
2025-04-08 10:20:19 +0200

    Process setup

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -4,7 +4,6 @@ from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConn
 from OpenOrchestrator.database.queues import QueueElement
 
 
-# pylint: disable-next=unused-argument
-def process(orchestrator_connection: OrchestratorConnection, queue_element: QueueElement | None = None) -> None:
+def process(orchestrator_connection: OrchestratorConnection) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
