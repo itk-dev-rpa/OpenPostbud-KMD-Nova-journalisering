12dbaceead8b46eb3f083ab61773afa5efa05a66
Mathias G <ghbm@aarhus.dk>
2026-04-16 10:05:36 +0200

    Implemented main process

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,19 +1,20 @@
 """This module contains the main process of the robot."""
 
-import base64
-from datetime import datetime
-import uuid
-from io import BytesIO
+import json
 
-import requests
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 from OpenOrchestrator.database.queues import QueueStatus
 from itk_dev_shared_components.kmd_nova.authentication import NovaAccess
-from itk_dev_shared_components.kmd_nova.nova_objects import NovaCase, Document, CaseParty, Task
-from itk_dev_shared_components.kmd_nova import nova_cases, nova_documents, nova_tasks
-from itk_dev_shared_components.kmd_nova import cpr as nova_cpr
+from itk_dev_shared_components.graph import authentication as graph_authentication
+from itk_dev_shared_components.graph.mail import Email
+from itk_dev_shared_components.graph.authentication import GraphAccess
+from itk_dev_shared_components.graph import mail as graph_mail
 
 from robot_framework import config
+from robot_framework.sub_process import mail_process, nova_process, open_postbud_process
+
+
+task_count = 0
 
 
 def process(orchestrator_connection: OrchestratorConnection) -> None:
@@ -21,139 +22,95 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     orchestrator_connection.log_trace("Running process.")
 
     openpostbud_key = orchestrator_connection.get_credential(config.OPENPOSTBUD_KEY).password
-    headers = {"X-API-key": openpostbud_key}
-    domain = orchestrator_connection.get_constant(config.OPENPOSTBUD_DOMAIN)
+    op_headers = {"X-API-key": openpostbud_key}
+    op_domain = orchestrator_connection.get_constant(config.OPENPOSTBUD_DOMAIN).value
 
     nova_creds = orchestrator_connection.get_credential(config.NOVA_API)
     nova_access = NovaAccess(nova_creds.username, nova_creds.password)
 
-    letter_ids = get_letter_ids("hej", headers, domain)
+    graph_credentials = orchestrator_connection.get_credential(config.GRAPH_API)
+    graph_access = graph_authentication.authorize_by_username_password(graph_credentials.username, **json.loads(graph_credentials.password))
 
-    for letter_id in letter_ids:
-        if not check_queue(letter_id, orchestrator_connection):
+    # Handle incoming emails and append queue
+    emails = mail_process.get_emails(graph_access)
+
+    for mail in emails:
+        handle_mail_request(mail, orchestrator_connection, graph_access, op_headers, op_domain)
+
+    # Handle queue
+    global task_count
+    while task_count < config.MAX_TASK_COUNT and (queue_element := orchestrator_connection.get_next_queue_element(config.QUEUE_NAME)):
+        task_count += 1
+        letter_id = queue_element.reference.split(":")[1]
+
+        json_data =json.loads(queue_element.data)
+        nova_params = mail_process.NovaParams(**json_data["nova_params"])
+        shipment_name = json_data["shipment_name"]
+
+        letter_status, recipient_cpr, letter_pdf = open_postbud_process.download_letter(letter_id, op_headers, op_domain)
+
+        if letter_status not in ("Afsendt", "Leveret"):
+            orchestrator_connection.set_queue_element_status(queue_element.id, status=QueueStatus.DONE, message=f"Letter has not been sent. Status: {letter_status}")
             continue
 
-        queue_element = orchestrator_connection.create_queue_element(config.QUEUE_NAME, reference=letter_id, created_by="Robot")
-        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.IN_PROGRESS)
+        if nova_params.reuse_case:
+            nova_case = nova_process.search_for_case(recipient_cpr, nova_params.case_title, nova_access)
+            if not nova_case:
+                error_message = f"No case with the title '{nova_params.case_title}' exists for '{recipient_cpr}'"
+                orchestrator_connection.set_queue_element_status(queue_element.id, status=QueueStatus.FAILED, message=error_message)
+                raise ValueError(error_message)
+        else:
+            nova_case = nova_process.create_case(recipient_cpr, nova_params, nova_access)
 
-        letter_pdf = download_letter(letter_id, headers, domain)
-        nova_case = create_case("Cpr", nova_access)
-        attach_letter_to_case(nova_case, letter_pdf, nova_access)
+        nova_process.attach_letter_to_case(nova_case, letter_pdf, shipment_name, nova_access)
 
-        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE)
-
-
-def get_letter_ids(shipment_id: str, headers: str, domain: str) -> list[str]:
-    """Get all the letter ids in the given OpenPostbud shipment.
-
-    Args:
-        shipment_id: The id of the shipment.
-        headers: The security header for OpenPostbud.
-        domain: The domain of OpenPostbud.
-
-    Returns:
-        A list of letter ids.
-    """
-    response = requests.get(f"{domain}/api/shipment/{shipment_id}", headers=headers, timeout=10)
-    response.raise_for_status()
-
-    return response.json()['letter_ids']
+        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE, message=f"Journalised at {nova_case.case_number}")
 
 
-def download_letter(letter_id: str, headers: str, domain: str) -> bytes:
-    """Get the merged pdf file for the given letter in OpenPostbud.
+def handle_mail_request(email: Email, orchestrator_connection: OrchestratorConnection, graph_access: GraphAccess, op_headers: dict[str, str], op_domain: str):
+    """Create queue elements based on an incoming email request.
 
     Args:
-        letter_id: The id of the letter.
-        headers: The security header for OpenPostbud.
-        domain: The domain of OpenPostbud.
-
-    Returns:
-        The merged pdf file as bytes.
+        email: The email request.
+        orchestrator_connection: The connection to Orchestrator.
+        graph_access: The access to Graph.
+        op_headers: The headers to use in OpenPostbud api calls.
+        op_domain: The domain of OpenPostbud.
     """
-    response = requests.get(f"{domain}/api/letter/{letter_id}", headers=headers, timeout=30)
-    response.raise_for_status()
+    shipment_id, nova_params, user = mail_process.html_to_params(email.body)
 
-    return base64.b64decode(response.json()['letter_pdf'])
+    if not mail_process.check_az(user.az, orchestrator_connection):
+        mail_process.send_rejection(user.email, shipment_id)
+        graph_mail.delete_email(email, graph_access)
+        return
 
+    # Create queue elements
+    shipment_name, letter_ids = open_postbud_process.get_letter_ids(shipment_id, op_headers, op_domain)
+    for letter_id in letter_ids:
+        reference = f"{shipment_id}:{letter_id}"
+        data = {
+            "nova_params": nova_params.to_dict(),
+            "shipment_name": shipment_name
+        }
 
-def check_queue(letter_id: str, orchestrator_connection: OrchestratorConnection) -> bool:
-    """Check if a letter has already been handled.
+        if not orchestrator_connection.get_queue_elements(config.QUEUE_NAME, reference=reference):
+            orchestrator_connection.create_queue_element(
+                queue_name=config.QUEUE_NAME,
+                reference=reference,
+                data=json.dumps(data, ensure_ascii=False),
+                created_by="Robot"
+            )
+        else:
+            raise ValueError(f"Queue element with reference '{reference}' already exists in queue.")
 
-    Args:
-        letter_id: The id of the letter.
-        orchestrator_connection: the connection to the Orchestrator.
-
-    Returns:
-        False if the letter is already in the queue.
-    """
-    queue_elements = orchestrator_connection.get_queue_elements(config.QUEUE_NAME, reference=letter_id)
-
-    if queue_elements:
-        return False
-
-    return True
-
-
-def create_case(cpr: str, nova_access: NovaAccess) -> NovaCase:
-    """Create a new case in Nova.
-
-    Args:
-        cpr: The cpr to create the case for.
-        nova_access: The access to the Nova api.
-
-    Returns:
-        The new case.
-    """
-    name = nova_cpr.get_address_by_cpr(cpr, nova_access)['name']
-
-    case_party = CaseParty(
-        role="Primær",
-        identification_type="CprNummer",
-        identification=cpr,
-        name=name,
-        uuid=None
-    )
-
-    # Create a new case
-    case = NovaCase(
-        uuid=str(uuid.uuid4()),
-        title=,
-        case_date=datetime.now(),
-        progress_state='Afsluttet',
-        case_parties=[case_party],
-        kle_number=,
-        proceeding_facet=,
-        sensitivity=,
-        caseworker=,
-        responsible_department=,
-        security_unit=
-    )
-
-    nova_cases.add_case(case, nova_access)
-    return case
+    mail_process.send_confirmation(user.email, shipment_id, len(letter_ids))
+    graph_mail.delete_email(email, graph_access)
 
 
-def attach_letter_to_case(case: NovaCase, letter_pdf: bytes, nova_access: NovaAccess):
-    """Attach the given letter as a document on the given Nova case.
+if __name__ == '__main__':
+    import os
 
-    Args:
-        case: The case in Nova.
-        letter_pdf: The letter as bytes.
-        nova_access: The access to the Nova api.
-    """
-    letter_io = BytesIO(letter_pdf)
-    doc_uuid = nova_documents.upload_document(letter_io, "Brev.pdf", nova_access)
-
-    doc = Document(
-        uuid=doc_uuid,
-        title="Brev",
-        sensitivity=,
-        document_type="Udgående",
-        document_date=datetime.now(),
-        approved=True,
-        description="Automatisk journaliseret af robot.",
-        caseworker=
-    )
-
-    nova_documents.attach_document_to_case(case.uuid, doc, nova_access)
+    conn_string = os.getenv("OpenOrchestratorConnString")
+    crypto_key = os.getenv("OpenOrchestratorKey")
+    oc = OrchestratorConnection("OpenPostbud journalisering", conn_string, crypto_key, '{"accepted_azs": ["az123456789"]}', "")
+    process(oc)
