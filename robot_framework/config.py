1d26e412977b4b5efd77dce19f2dace8c64b0f26
Mathias G <ghbm@aarhus.dk>
2026-04-16 10:01:24 +0200

    Updated config

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -1,5 +1,7 @@
 """This module contains configuration constants used across the framework"""
 
+from itk_dev_shared_components.kmd_nova.nova_objects import Caseworker
+
 # The number of times the robot retries on an error before terminating.
 MAX_RETRY_COUNT = 3
 
@@ -17,16 +19,46 @@ ERROR_EMAIL = "Error Email"
 OPENPOSTBUD_KEY = "OpenPostbud Key"
 OPENPOSTBUD_DOMAIN = "OpenPostbud Domain"
 
-GRAPH_API = "Graph API"
 NOVA_API = "Nova API"
+GRAPH_API = "Graph API"
 
-# Queue specific configs
-# ----------------------
-
-# The name of the job queue (if any)
 QUEUE_NAME = "OpenPostbud KMD Nova Journalisering"
+MAX_TASK_COUNT = 500
 
-# The limit on how many queue elements to process
-MAX_TASK_COUNT = 100
+# Config options
+MAIL_SOURCE_FOLDER = "Indbakke/OpenPostbud Nova journalisering"
+MAIL_INBOX_SUBJECT = "RPA - OpenPostbud journalisering i Nova (fra Selvbetjening.aarhuskommune.dk)"
+STATUS_SENDER = "itk-rpa@ba.aarhus.dk"
 
-# ----------------------
+
+# KMD Constants
+KMD_DEPARTMENTS = {
+    "4BFOLKEREG": {"name": "Folkeregister og Sygesikring", "id": "70403"},
+    "4BBORGER": {"name": "Borgerservice", "id": "818485"},
+    "4BFRONT": {"name": "Frontbetjening", "id": "1061417"},
+    "4BFRONTTEL": {"name": "Kontaktcentret", "id": "1061418"},
+    "4BKONTROL": {"name": "Kontrolteamet", "id": "70363"},
+    "4BOPKRÆV": {"name": "Opkrævningen", "id": "70391"},
+}
+
+KMD_DEPARTMENT_SECURITY_PAIRS = {
+    "4BFOLKEREG": "4BBORGER",
+    "4BBORGER": "4BBORGER",
+    "4BFRONT": "4BBORGER",
+    "4BFRONTTEL": "4BBORGER",
+    "4BKONTROL": "4BKONTROL",
+    "4BOPKRÆV": "4BOPKRÆV",
+}
+
+KMD_SENSITIVITY = {
+    "Fortrolige oplysninger": "Fortrolige",
+    "Ikke fortrolige oplysninger": "IkkeFortrolige",
+    "Særligt følsomme oplysninger": "SærligFølsomme",
+    "Følsomme oplysninger": "Følsomme"
+}
+
+CASEWORKER = Caseworker(
+    uuid="3eb31cbb-d6dc-4b01-9eec-0b415f5b89cb",
+    name="Rpabruger Rpa15 - MÅ IKKE SLETTES RITM0055928",
+    ident="AZRPA15"
+)
